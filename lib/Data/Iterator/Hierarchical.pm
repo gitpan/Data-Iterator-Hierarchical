8861c3de2e6f9da4554874ed5ac1a6d7157d290a
Brian McCauley <nobull67@gmail.com>
2008-11-02 13:11:36 +0000

    Import of NOBULL/Data-Iterator-Hierarchical-0.06 from CPAN.
    gitpan-cpan-distribution: Data-Iterator-Hierarchical
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         NOBULL/Data-Iterator-Hierarchical-0.06.tar.gz
    gitpan-cpan-author:       NOBULL
    gitpan-cpan-maturity:     released

--- lib/Data/Iterator/Hierarchical.pm
+++ lib/Data/Iterator/Hierarchical.pm
@@ -9,11 +9,11 @@ Data::Iterator::Hierarchical - Iterate hierarchically over tabular data
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 
 =head1 SYNOPSIS
